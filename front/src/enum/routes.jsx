8c275ab0b428751204b4a0e3ceabd9f977d2371f
euion <qq212420@gmail.com>
2022-11-22 12:20:53 +0900

    feat:커뮤니티초기환경 구축

--- front/src/enum/routes.jsx
+++ front/src/enum/routes.jsx
@@ -7,4 +7,10 @@ export const ROUTES = {
   },
   USER: { LOGIN: "/login", REGISTER: "/register" },
   PERSONAL: "/personal",
+  COMMUNITY: {
+    ROOT: "/community",
+    COMMUNITY_CREATE: "/community/communityCreate",
+    COMMUNITY_DETAIL: "/community/communityDetail",
+    COMMUNITY_EDIT: "/community/communityEdit",
+  },
 };
---
+++ front/src/pages/CommunityEditPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommunityEditPage = (): JSX.Element => {
+  return (
+    <div className="CommunityEditPage">
+      <p> 커뮤니티 수정 페이지</p>
+    </div>
+  );
+};
+
+export default CommunityEditPage;
