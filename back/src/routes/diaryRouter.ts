f0b64a1e358184e3f1e90ac108e76c8960dcf65b
임지원 <9universe9th@gmail.com>
2022-11-24 23:11:51 +0900

    fix: diary api 3계층 분리

--- back/src/routes/diaryRouter.ts
+++ back/src/routes/diaryRouter.ts
@@ -1,64 +1,12 @@
-import { Router, Request, Response } from "express";
-import DiaryService from "services/diaryService";
+import { Router } from "express";
+import DiaryController from "controllers/diaryController";
+
 const diaryRouter: Router = Router();
 
-//일기 작성하기
-diaryRouter.post("/:userId/diaries", async (req: Request, res: Response) => {
-  try {
-    const userId = req.params.userId;
-    const { title, description, emotion } = req.body;
-    const result = await DiaryService.createDiary(
-      userId,
-      title,
-      description,
-      emotion
-    );
-    res.status(200).send(result);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-//일기 가져오기
-diaryRouter.get(
-  "/:userId?/diaries/:id?",
-  async (req: Request, res: Response) => {
-    try {
-      const { userId, id } = req.params;
-      const result = await DiaryService.getDiary(userId, id);
-      res.status(200).send(result);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-);
-
-//일기 수정하기
-diaryRouter.put("/diaries/:id", async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const { title, description, emotion } = req.body;
-    const result = await DiaryService.editDiary(
-      id,
-      title,
-      description,
-      emotion
-    );
-    res.status(200).send(result);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-//일기 삭제하기
-diaryRouter.delete("/diaries/:id", async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const result = await DiaryService.deleteDiary(id);
-    res.status(200).send(result);
-  } catch (error) {
-    console.log(error);
-  }
-});
+diaryRouter.post("/:userId/diaries", DiaryController.createDiary);
+diaryRouter.get("/:userId/diaries", DiaryController.getDiaries);
+diaryRouter.get("/diaries/:id", DiaryController.getDiaryById);
+diaryRouter.put("/diaries/:id", DiaryController.updateDiary);
+diaryRouter.delete("/diaries/:id", DiaryController.deleteDiary);
 
 export default diaryRouter;
--- back/src/services/diaryService.ts
+++ back/src/services/diaryService.ts
@@ -3,17 +3,10 @@ import { PrismaClient } from "@prisma/client";
 class DiaryService {
   prisma = new PrismaClient();
 
-  async createDiary(
-    userId: string,
-    title: string,
-    description: string,
-    emotion: string
-  ) {
+  async createDiary(userId: string, diaryData) {
     const result = await this.prisma.diary.create({
       data: {
-        title,
-        description,
-        emotion,
+        ...diaryData,
         user: {
           connect: {
             userId,
@@ -24,32 +17,25 @@ class DiaryService {
     await this.prisma.$disconnect();
     return result;
   }
-  async getDiary(userId: string, id: string) {
-    if (!id) {
-      const result = await this.prisma.diary.findMany({
-        where: { userId },
-      });
-      await this.prisma.$disconnect();
-      return result;
-    }
-    if (!userId) {
-      const result = await this.prisma.diary.findUnique({
-        where: { id: Number(id) },
-      });
-      await this.prisma.$disconnect();
-      return result;
-    }
+  async findDiaries(userId: string) {
+    const result = await this.prisma.diary.findMany({
+      where: { userId },
+    });
+    await this.prisma.$disconnect();
+    return result;
+  }
+  async findDiaryById(id: string) {
+    const result = await this.prisma.diary.findUnique({
+      where: { id: Number(id) },
+    });
+    await this.prisma.$disconnect();
+    return result;
   }
 
-  async editDiary(
-    id: string,
-    title: string,
-    description: string,
-    emotion: string
-  ) {
+  async updateDiary(id: string, diaryData) {
     const result = await this.prisma.diary.update({
       where: { id: Number(id) },
-      data: { title, description, emotion },
+      data: { ...diaryData },
     });
     await this.prisma.$disconnect();
     return result;
