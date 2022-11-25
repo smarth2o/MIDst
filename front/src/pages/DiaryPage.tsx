aed5b0dcffad62f1174457ded09d1f94fcef36ee
euion <qq212420@gmail.com>
2022-11-25 21:37:11 +0900

    fix:메인브런치충돌해결

--- front/src/pages/DiaryPage.tsx
+++ front/src/pages/DiaryPage.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import DiaryBottom from "../components/diary/DiaryBottom";
+import DiaryCreate from "../components/diary/DiaryCreate";
+import { DiarySide } from "../components/diary/DiarySide";
+import { DiaryCreateAlign } from "../styles/diary/DiaryCreate";
+import {
+  AllBackGround,
+  DiaryAlign,
+  DiaryMain,
+} from "../styles/diary/DiaryCreatePage";
 
 const DiaryPage = (): JSX.Element => {
   return (
     <div className="DiaryPage">
-      <p> 일기 렌딩 페이지</p>
+      <AllBackGround>
+        <DiaryAlign>
+          <DiarySide />
+          <DiaryMain>
+            <DiaryCreateAlign></DiaryCreateAlign>
+            <DiaryBottom />
+          </DiaryMain>
+        </DiaryAlign>
+      </AllBackGround>
     </div>
   );
 };
