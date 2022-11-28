426b0bb735c3e30f713d5c248843ee53d275138c
euion <qq212420@gmail.com>
2022-11-28 22:04:18 +0900

    feat:nav바,PBCBottomCard배열출력수정

---
+++ front/src/styles/diary/DiaryPage.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CreateDiaryBtn = styled.button`
+  width: 50px;
+  max-height: 50px;
+  height: 100%;
+  border: solid 3px #6e48eb7e;
+  border-radius: 50px;
+  color: #6f48eb;
+  a {
+    text-decoration: none;
+    font-size: 25px;
+    color: #6e48eb7e;
+  }
+`;
