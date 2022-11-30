3f7e4d4f0931f8c0825508b2381978fa23e2eb3d
임지원 <9universe9th@gmail.com>
2022-12-01 02:32:19 +0900

    fix:host

--- back/src/swagger.js
+++ back/src/swagger.js
@@ -1,24 +1,24 @@
 const swaggerAutogen = require("swagger-autogen")();
 
 const options = {
-  info: {
-    title: "6dan",
-    description: "6dan api명세",
-  },
-  servers: [
-    {
-      url: "http://localhost:8080",
+    info: {
+        title: "6dan",
+        description: "6dan api명세",
     },
-  ],
-  schemes: ["http"],
-  securityDefinitions: {
-    bearerAuth: {
-      type: "http",
-      scheme: "bearer",
-      in: "header",
-      bearerFormat: "JWT",
+    host: "localhost:8080",
+    servers: [
+        {
+            url: "http://localhost:8080",
+        },
+    ],
+    schemes: ["http"],
+    securityDefinitions: {
+        jwt: {
+            type: "apiKey",
+            name: "authorization",
+            in: "header",
+        },
     },
-  },
 };
 
 const outputFile = "./swagger_output.json";
