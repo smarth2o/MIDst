02214793bb71e55c486854844a029e6f7f968f2c
임지원 <9universe9th@gmail.com>
2022-12-02 20:43:55 +0900

    feat: morgan

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,21 +1,25 @@
 import express from "express";
-import path from "path";
 import cors from "cors";
+import morgan from "morgan";
 import { router } from "./routes/index";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger_output.json";
+import errorMiddleware from "middlewares/errorMiddleware";
 
 export const app = express();
+
+app.use(morgan("dev"));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
+app.use(errorMiddleware);
 
 app.use(
-  "/doc",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerFile, { explorer: true })
+    "/doc",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, { explorer: true })
 );
