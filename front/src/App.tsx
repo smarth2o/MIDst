f4da40b5b79d5918ab074acc76271aa914e63957
김현정 <khjung11203@gmail.com>
2022-12-05 20:50:16 +0900

    fix: add pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ROUTES } from "./enum/routes";
 import { GlobalStyle } from "./styles/GlobalStyle.styled";
 import Header from "./components/Header";
-import CommunityCreatePage from "./pages/CommunityCreatePage";
-import CommunityDetailPage from "./pages/CommunityDetailPage";
-import CommunityEditPage from "./pages/CommunityEditPage";
-import CommunityPage from "./pages/CommunityPage";
-import DiaryPage from "./pages/DiaryPage";
-import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
-import PersonalPage from "./pages/PersonalPage";
-import RegisterPage from "./pages/RegisterPage";
+import SearchPage from "./pages/SearchPage";
+import CommunityPage from "./pages/CommunityPage";
+import CommunityDetailPage from "./pages/CommunityDetailPage";
+import CommunityCreatePage from "./pages/CommunityCreatePage";
+import CommunityEditPage from "./pages/CommunityEditPage";
+import DiaryPage from "./pages/DiaryPage";
 import DiaryDetail from "./components/diary/DiaryDetail";
 import DiaryCreate from "./components/diary/DiaryCreate";
+import PersonalPage from "./pages/PersonalPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import FindPasswordPage from "./pages/FindPasswordPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
@@ -26,14 +30,7 @@ const App = (): JSX.Element => {
         <Routes>
           <Route path={ROUTES.MAIN} element={<MainPage />} />
           <Route element={<Header />}>
-            <Route path={ROUTES.DIARY.ROOT} element={<DiaryPage />}>
-              <Route path={ROUTES.DIARY.DETAIL} element={<DiaryDetail/>} />
-              <Route path={ROUTES.DIARY.CREATE} element={<DiaryCreate />} />
-            </Route>
-
-            <Route path={ROUTES.USER.LOGIN} element={<LoginPage />} />
-            <Route path={ROUTES.USER.REGISTER} element={<RegisterPage />} />
-            <Route path={ROUTES.PERSONAL} element={<PersonalPage />} />
+            <Route path={ROUTES.SEARCH} element={<SearchPage />} />
             <Route path={ROUTES.COMMUNITY.ROOT} element={<CommunityPage />} />
             <Route
               path={ROUTES.COMMUNITY.COMMUNITY_CREATE}
@@ -47,7 +44,20 @@ const App = (): JSX.Element => {
               path={ROUTES.COMMUNITY.COMMUNITY_EDIT}
               element={<CommunityEditPage />}
             />
-            <Route path="/*">Not Found</Route>
+            <Route path={ROUTES.DIARY.ROOT} element={<DiaryPage />}>
+              <Route path={ROUTES.DIARY.DETAIL} element={<DiaryDetail />} />
+              <Route path={ROUTES.DIARY.CREATE} element={<DiaryCreate />} />
+            </Route>
+            <Route path={ROUTES.PERSONAL} element={<PersonalPage />} />
+            <Route path={ROUTES.USER.LOGIN} element={<LoginPage />} />
+            <Route path={ROUTES.USER.REGISTER} element={<RegisterPage />} />
+            <Route path={ROUTES.USER.FIND_PW} element={<FindPasswordPage />} />
+            <Route
+              path={ROUTES.USER.CHANGE_PW}
+              element={<ChangePasswordPage />}
+            />
+
+            <Route path="/*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </Router>
--- front/src/enum/routes.jsx
+++ front/src/enum/routes.jsx
@@ -1,22 +1,22 @@
 export const ROUTES = {
   MAIN: "/",
   SEARCH: "/search",
-  DIARY: {
-    ROOT: "/diary",
-    DETAIL: ":detail",
-    CREATE: "create",
-  },
-  USER: {
-    LOGIN: "/login",
-    REGISTER: "/register",
-    FIND_PW: "/findPassword",
-    CHANGE_PW: "/changePassword",
-  },
-  PERSONAL: "/personal",
   COMMUNITY: {
     ROOT: "/community",
     COMMUNITY_CREATE: "/community/communityCreate",
     COMMUNITY_DETAIL: "/community/communityDetail",
     COMMUNITY_EDIT: "/community/communityEdit",
+  },
+  DIARY: {
+    ROOT: "/diary",
+    DETAIL: ":detail",
+    CREATE: "create",
+  },
+  PERSONAL: "/personal",
+  USER: {
+    LOGIN: "/login",
+    REGISTER: "/register",
+    FIND_PW: "/findPassword",
+    CHANGE_PW: "/changePassword",
   },
 };
