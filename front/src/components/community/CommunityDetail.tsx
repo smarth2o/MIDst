ff1a7670d4c1048bc69b8142c12f94292ffe8c99
euion <qq212420@gmail.com>
2022-12-08 14:35:12 +0900

    fix:커뮤니티병합충돌수정

--- front/src/components/community/CommunityDetail.tsx
+++ front/src/components/community/CommunityDetail.tsx
@@ -1,33 +1,52 @@
-import { count } from "console";
-import { useOutletContext, useParams } from "react-router";
-import { DiaryValueType } from "../../pages/DiaryPage";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { useNavigate, useOutletContext, useParams } from "react-router";
 import { CommunityType } from "../../stores/CommunityAtom";
-import { CommunityPostBoxStyled } from "../../styles/community/CommunityDetailPost";
-import CommunityComment from "./CommunityComment";
-import { CommunityPropsTypes } from "./CommunityList";
+import { BackBtnAlignStyled } from "../../styles/common/CommonBtn";
+import {
+  CommunityCommentStyled,
+  CommunityPostBoxStyled,
+} from "../../styles/community/CommunityDetailPost";
+import CommunityComment from "./CommunityReply";
+import { CommunityPropsType } from "./CommunityList";
 import CommunityPost from "./CommunityPost";
 
 const CommunityDetail = (): JSX.Element => {
-  const { communityItems, setCommunityItems } = useOutletContext<CommunityPropsTypes>();
-  const {detail} = useParams();
-  const currentIndex = Number(detail ?? 1);
+  const { communityItems, setCommunityItems } =
+    useOutletContext<CommunityPropsType>();
+  const { communityDetail } = useParams();
+  const currentIndex = Number(communityDetail ?? 1);
   const currentCommunityItem = communityItems.find(
-    (communityItem) => communityItem.id === currentIndex 
+    (communityItem) => communityItem.id === currentIndex
   ) as CommunityType;
-  
-  
+  const navigate = useNavigate();
+  const onClickBtn = () => {
+    navigate(-1);
+  };
+
   return (
     <>
+      <BackBtnAlignStyled>
+        <button className="back-btn" onClick={onClickBtn}>
+          <ArrowLeftOutlined />
+          돌아가기
+        </button>
+      </BackBtnAlignStyled>
+
       <CommunityPostBoxStyled>
-        <CommunityPost 
-        id={currentIndex}
-        title={currentCommunityItem.title}
-        description = {currentCommunityItem.description}
-        createdAt={currentCommunityItem.createdAt}
-count= {currentCommunityItem.count}
+        <CommunityPost
+          id={currentIndex}
+          title={currentCommunityItem.title}
+          description={currentCommunityItem.description}
+          createdAt={currentCommunityItem.createdAt}
+          reply={currentCommunityItem.count.reply}
+          like={currentCommunityItem.count.like}
+          communityItems={communityItems}
+          setCommunityItems={setCommunityItems}
         />
       </CommunityPostBoxStyled>
-      <CommunityComment/>
+      <CommunityCommentStyled>
+        <CommunityComment />
+      </CommunityCommentStyled>
     </>
   );
 };
