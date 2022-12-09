432134188071123fb2bbc4c1409ab218eff70a98
임지원 <9universe9th@gmail.com>
2022-12-09 21:32:39 +0900

    fix: db 오류

--- back/src/repositories/search.repository.ts
+++ back/src/repositories/search.repository.ts
@@ -1,54 +1,50 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-class mainController{
-    
+class mainController {
     //문장 저장
-    async create(userId:string,searchword:string,searchSentence:string){
+    async create(userId: string, searchword: string, searchSentence: string) {
         console.log(userId);
-        const userTosearch=await prisma.usertosearch.create({
-            data:{
-                userId:userId,
-                searchWord:searchword,
-                searchSentence:searchSentence
-            }
-        })
-        return userTosearch
+        const userTosearch = await prisma.usertosearch.create({
+            data: {
+                userId: userId,
+                searchWord: searchword,
+                description: searchSentence,
+            },
+        });
+        return userTosearch;
     }
     //저장한거 가져오기
-    async find(userId:string){
-        const userTosearch=await prisma.usertosearch.findMany({
-            where:{
-                userId:userId
+    async find(userId: string) {
+        const userTosearch = await prisma.usertosearch.findMany({
+            where: {
+                userId: userId,
             },
-        })
+        });
         return userTosearch;
     }
 
     //총 저장 개수
-    async count(userId:string){
-    const count = await prisma.user.findMany({
-        where: { 
-            userId:userId, 
-        },
-        select: {
-            _count: { select: { usertosearch: true } },
+    async count(userId: string) {
+        const count = await prisma.user.findMany({
+            where: {
+                userId: userId,
             },
-
-      });
-      return count;
+            select: {
+                _count: { select: { usertosearch: true } },
+            },
+        });
+        return count;
     }
 
     //저장 삭제
-    async delete(searchId:number){
+    async delete(searchId: number) {
         const result = await prisma.usertosearch.delete({
             where: {
-                searchId:searchId, 
+                searchId: searchId,
             },
-          });
-          return result;
-        }
-      
-    
+        });
+        return result;
+    }
 }
 export default new mainController();
