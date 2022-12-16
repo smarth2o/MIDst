c8e9ddfa55562c947b873cdc221d3885994ca4de
임지원 <9universe9th@gmail.com>
2022-12-17 00:13:19 +0900

    fix: plus button

--- front/src/components/community/CommunityList.tsx
+++ front/src/components/community/CommunityList.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useState } from "react";
 import { DownOutlined } from "@ant-design/icons";
 import {
-  CommunityAllAlignStyled,
-  CommunityListAlignStyled,
-  CommunityListStyled,
-  CommunitySortBtnStyled,
-  CommunityWriteBtnStyled,
+    CommunityAllAlignStyled,
+    CommunityListAlignStyled,
+    CommunityListStyled,
+    CommunitySortBtnStyled,
+    CommunityWriteBtnStyled,
 } from "../../styles/community/CommunityList";
 
 import { communityState, CommunityType } from "../../stores/CommunityAtom";
@@ -17,89 +17,105 @@ import * as Api from "../../api";
 import CommunityItem from "./CommunityItem";
 
 export interface CommunityPropsType {
-  id: number;
-  author: string;
-  title: string;
-  createdAt: string;
-  description: string;
-  reply: number;
-  like: number;
-  communityItems: CommunityType[];
+    id: number;
+    author: string;
+    title: string;
+    createdAt: string;
+    description: string;
+    reply: number;
+    like: number;
+    communityItems: CommunityType[];
 }
 
 const CommunityList = (): JSX.Element => {
-  const [communityItems, setCommunityItems] = useRecoilState(communityState);
-  const [urlValue, setUrlValue] = useState(`newest`);
+    const [communityItems, setCommunityItems] = useRecoilState(communityState);
+    const [urlValue, setUrlValue] = useState(`newest`);
 
-  useEffect(() => {
-    const CommunityData = async () => {
-      const response = await Api.get(`posts/all/${urlValue}`);
-      if (response.status !== 200) {
-        console.log(response);
-      } else {
-        setCommunityItems(response.data.data);
-      }
-    };
-    CommunityData();
-  }, [urlValue]);
+    useEffect(() => {
+        const CommunityData = async () => {
+            const response = await Api.get(`posts/all/${urlValue}`);
+            if (response.status !== 200) {
+                console.log(response);
+            } else {
+                setCommunityItems(response.data.data);
+            }
+        };
+        CommunityData();
+    }, [urlValue]);
 
-  return (
-    <>
-      <CommunityAllAlignStyled>
-        <CommunitySortBtnStyled>
-          <button
-            onClick={() => {
-              setUrlValue(`popular`);
-            }}
-          >
-            <p>
-              좋아요 순 <DownOutlined />
-            </p>
-          </button>
-          <button
-            onClick={() => {
-              setUrlValue(`newest`);
-            }}
-          >
-            <p>
-              최신 순 <DownOutlined />
-            </p>
-          </button>
-        </CommunitySortBtnStyled>
-        <CommunityListAlignStyled>
-          <CommunityListStyled>
-            {communityItems &&
-              communityItems.map((communityItem: CommunityType) => {
-                const { id, title, createdAt, author, description, _count } =
-                  communityItem;
-                return (
-                  <div key={id}>
-                    <Link to={`/community/${id}`} className="community-link">
-                      <CommunityItem
-                        id={id}
-                        author={author}
-                        title={title}
-                        createdAt={createdAt}
-                        description={description}
-                        reply={_count.reply}
-                        like={_count.like}
-                        communityItems={communityItems}
-                      />
+    return (
+        <>
+            <CommunityAllAlignStyled>
+                <CommunitySortBtnStyled>
+                    <button
+                        onClick={() => {
+                            setUrlValue(`popular`);
+                        }}
+                    >
+                        <p>
+                            좋아요 순 <DownOutlined />
+                        </p>
+                    </button>
+                    <button
+                        onClick={() => {
+                            setUrlValue(`newest`);
+                        }}
+                    >
+                        <p>
+                            최신 순 <DownOutlined />
+                        </p>
+                    </button>
+                    <Link to={ROUTES.COMMUNITY.CREATE}>
+                        <button className="plus">+</button>
                     </Link>
-                  </div>
-                );
-              })}
-          </CommunityListStyled>
-        </CommunityListAlignStyled>
+                </CommunitySortBtnStyled>
+                <CommunityListAlignStyled>
+                    <CommunityListStyled>
+                        {communityItems &&
+                            communityItems.map(
+                                (communityItem: CommunityType) => {
+                                    const {
+                                        id,
+                                        title,
+                                        createdAt,
+                                        author,
+                                        description,
+                                        _count,
+                                    } = communityItem;
+                                    return (
+                                        <div key={id}>
+                                            <Link
+                                                to={`/community/${id}`}
+                                                className="community-link"
+                                            >
+                                                <CommunityItem
+                                                    id={id}
+                                                    author={author}
+                                                    title={title}
+                                                    createdAt={createdAt}
+                                                    description={description}
+                                                    reply={_count.reply}
+                                                    like={_count.like}
+                                                    communityItems={
+                                                        communityItems
+                                                    }
+                                                />
+                                            </Link>
+                                        </div>
+                                    );
+                                }
+                            )}
+                    </CommunityListStyled>
+                </CommunityListAlignStyled>
 
-        <CommunityWriteBtnStyled>
-          <Link to={ROUTES.COMMUNITY.CREATE}>
-            <button>Write</button>
-          </Link>
-        </CommunityWriteBtnStyled>
-      </CommunityAllAlignStyled>
-    </>
-  );
+                <CommunityWriteBtnStyled>
+                    <Link to={ROUTES.COMMUNITY.CREATE}>
+                        <button>Write</button>
+                    </Link>
+                </CommunityWriteBtnStyled>
+            </CommunityAllAlignStyled>
+        </>
+    );
 };
 
 export default CommunityList;
--- front/src/styles/community/CommunityList.tsx
+++ front/src/styles/community/CommunityList.tsx
@@ -1,95 +1,113 @@
 import styled from "styled-components";
 
 export const CommunityListStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  width: 80vw;
-  padding: 20px;
-  .community-link {
-    text-decoration: none;
-    color: black;
-  }
-
-  div {
-    @media all and (min-width: 1300px) {
-      width: 70vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 80vw;
+    padding: 20px;
+    .community-link {
+        text-decoration: none;
+        color: black;
     }
-  }
+
+    div {
+        @media all and (min-width: 1300px) {
+            width: 70vw;
+        }
+    }
 `;
 
 export const CommunityCardAlignStyled = styled.div`
-  background-color: white;
-  height: 100px;
-  border-radius: 20px;
-  padding: 4%;
-  margin-bottom: 20px;
-  img {
-    border-radius: 50px;
-    width: 40px;
-  }
-  li {
-    list-style: none;
-  }
+    background-color: white;
+    height: 100px;
+    border-radius: 20px;
+    padding: 4%;
+    margin-bottom: 20px;
+    img {
+        border-radius: 50px;
+        width: 40px;
+    }
+    li {
+        list-style: none;
+    }
 `;
 
 export const CommunityInfo = styled.div`
-  ul {
-    padding: 0;
-    display: flex;
-  }
-  ul li {
-    padding: 10px;
-  }
+    ul {
+        padding: 0;
+        display: flex;
+    }
+    ul li {
+        padding: 10px;
+    }
 `;
 
 export const CommunityWriteBtnStyled = styled.div`
-  display: flex;
-  justify-content: space-around;
-  button {
-    width: 165px;
-    height: 42px;
-    padding: 10px 15px;
-    gap: 2px;
-    background: #ffffff;
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
-    border-radius: 20px;
-    border: none;
-    cursor: pointer;
-  }
+    display: flex;
+    justify-content: space-around;
+    button {
+        width: 165px;
+        height: 42px;
+        padding: 10px 15px;
+        gap: 2px;
+        background: #ffffff;
+        box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
+        border: none;
+        cursor: pointer;
+    }
 `;
 
 export const CommunitySortBtnStyled = styled.div`
-  padding-top: 2%;
-  display: grid;
-  grid-template-columns: 150px 150px;
-  align-items: start;
-  button {
-    margin-left: 0%;
-    width: 121px;
-    height: 42px;
-    align-items: center;
-    padding: 0px 15px;
-    gap: 2px;
-    background: #ffffff;
-    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
-    border-radius: 20px;
-    border: none;
+    padding-top: 2%;
+    display: grid;
+    grid-template-columns: 150px 150px 50px;
+    align-items: start;
+    button {
+        margin-left: 0%;
+        width: 121px;
+        height: 42px;
+        align-items: center;
+        padding: 0px 15px;
+        gap: 2px;
+        background: #ffffff;
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
+        border: none;
 
-    cursor: pointer;
-    :hover {
-      background: #d6d6d6;
+        cursor: pointer;
+        :hover {
+            background: #d6d6d6;
+        }
     }
-  }
+
+    .plus {
+        margin-left: 0%;
+        width: 40px;
+        height: 40px;
+        align-items: center;
+        padding: 0px 15px;
+        gap: 2px;
+        background: #a59ce1;
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
+        border-radius: 50px;
+        border: none;
+
+        cursor: pointer;
+        :hover {
+            background: #7ec9ca;
+        }
+    }
 `;
 
 export const CommunityAllAlignStyled = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
 `;
 
 export const CommunityListAlignStyled = styled.div`
-  display: flex;
-  justify-content: space-around;
+    display: flex;
+    justify-content: space-around;
 `;
